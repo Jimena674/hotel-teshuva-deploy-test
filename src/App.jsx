ad49794c7ee39eb6ee734a6bbb0ebd71991e695f
Jimena674 <mjsp674@hotmail.com>
2025-03-31 21:34:09 -0500

    Se realizan pruebas iniciales con el componente button

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Button from "./webpage/components/Button.jsx";
 function App() {
   return (
     <>
-      <Button name="Iniciar Sesión" />
+      <Button name="Iniciar Sesión" className="btn-primary" />
     </>
   );
 }
--- src/webpage/components/Button.jsx
+++ src/webpage/components/Button.jsx
@@ -1,8 +1,8 @@
-export default function Button({ name }) {
+export default function Button(props) {
   // This is a simple button component that takes a name prop and renders a button with that name
   return (
-    <button type="button" className="btn">
-      {name}
+    <button type="button" className="btn {props.className}">
+      {props.name}
     </button>
   );
 }
