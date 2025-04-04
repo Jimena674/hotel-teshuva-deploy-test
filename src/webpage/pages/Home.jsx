7287178ced199ecc98d07ea3f410f883e034a8b8
Jimena Sánchez <mjsp674@hotmail.com>
2025-04-04 16:40:19 -0500

    Se crea el CardITB básicamnte.

--- src/webpage/pages/Home.jsx
+++ src/webpage/pages/Home.jsx
@@ -1,12 +1,14 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import BookingForm from "../components/BookingForm";
+import CardITB from "../components/CardITB";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <BookingForm />
+      <CardITB />
       <Footer />
     </>
   );
