6194442c4e40f3edcede3cdd5e52b7e6dd7af262
Jimena Sánchez <mjsp674@hotmail.com>
2025-04-16 11:28:01 -0500

    Se instala react-date-range y date-fns. Se ajusta el bookingForm.

--- src/components/BookingForm.jsx
+++ src/components/BookingForm.jsx
@@ -1,7 +1,26 @@
-import { Link } from "react-router-dom";
 import Button from "./Button";
+import { DateRange } from "react-date-range";
+//Importar los estilos y esqueleto para DateRange
+import "react-date-range/dist/styles.css"; // main style file
+import "react-date-range/dist/theme/default.css"; // theme css file
+import { useState } from "react";
 
 export default function BookingForm() {
+  //Se declaran los estados y eventos necesarios
+  const [range, setRange] = useState([
+    {
+      startDate: new Date(),
+      endDate: new Date(),
+      key: "selection",
+    },
+  ]);
+
+  const [showCalendar, setShowCalendar] = useState(false);
+
+  const toggleCalendar = () => {
+    setShowCalendar(!showCalendar);
+  };
+
   return (
     <>
       {/*Contenedor principal que centra todo*/}
@@ -17,103 +36,124 @@ export default function BookingForm() {
               Hacer una Reserva
             </span>
           </div>
-          {/*Franja de botones*/}
-          <div className="d-flex flex-column flex-md-row justify-content-center">
-            {/*Botón de check-in y check-out*/}
 
-            <div className="d-flex flex-row booking-form-btn px-2 py-0">
-              {/*Check-in*/}
-              <div className="d-flex flex-column me-3">
-                <div className="d-flex flex-row">
-                  <div className="d-flex flex-column me-3 justify-content-center">
-                    <i
-                      className="bi bi-calendar"
-                      style={{ fontSize: "1.5rem" }}
-                    />
-                  </div>
-                  <div className="d-flex flex-column text-start">
-                    <div className="row mb-0">
-                      <span className="label-small">Check-in</span>
+          {/*Franja de botones*/}
+          <div
+            className="d-flex flex-column flex-md-row justify-content-center align-self-center"
+            style={{ width: "fit-content" }}
+          >
+            {/*Botón de check-in y check-out*/}
+            <div className="d-flex justify-content-center mt-2 mt-md-0">
+              <div className="position-relative">
+                <div
+                  className="d-flex justify-content-center booking-form-btn input-group input-custom"
+                  onClick={toggleCalendar}
+                >
+                  {/*Check-in*/}
+
+                  <div className="d-flex flex-row me-3">
+                    <div className="d-flex flex-column me-3 justify-content-center">
+                      <i
+                        className="bi bi-calendar"
+                        style={{ fontSize: "1.5rem" }}
+                      />
                     </div>
-                    <div className="row mb-0">
-                      <span className="body-small">Añadir fecha</span>
+                    <div className="d-flex flex-column text-start">
+                      <div className="row ">
+                        <span className="label-small">Check-in</span>
+                      </div>
+                      <div className="row ">
+                        <span className="body-small">
+                          {range[0].startDate.toLocaleDateString()}
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+
+                  {/*Check-out*/}
+                  <div className="d-flex flex-column border-start ps-3">
+                    <div className="d-flex flex-row">
+                      <div className="d-flex flex-column me-3 justify-content-center">
+                        <i
+                          className="bi bi-calendar"
+                          style={{ fontSize: "1.5rem" }}
+                        />
+                      </div>
+                      <div className="d-flex flex-column text-start">
+                        <div className="row fw-bold">
+                          <span className="label-small">Check-out</span>
+                        </div>
+                        <div className="row">
+                          <span className="body-small">
+                            {range[0].startDate.toLocaleDateString()}
+                          </span>
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </div>
+                {showCalendar && (
+                  <div
+                    className="position-absolute z-3"
+                    style={{ top: "100%", left: 0 }}
+                  >
+                    <DateRange
+                      editableDateInputs={true}
+                      onChange={(item) => setRange([item.selection])}
+                      moveRangeOnFirstSelection={false}
+                      ranges={range}
+                    />
+                  </div>
+                )}
               </div>
-              {/*Check-out*/}
-              <div className="d-flex flex-column border-start ps-3">
+            </div>
+
+            {/*Botón de Habitaciones y Huéspedes*/}
+            <div className="d-flex justify-content-center mx-md-3 mt-2 mt-md-0">
+              <div className="d-flex justify-content-center booking-form-btn input-group input-custom border">
+                {/*Botón de Habitaciones*/}
                 <div className="d-flex flex-row">
                   <div className="d-flex flex-column me-3 justify-content-center">
                     <i
-                      className="bi bi-calendar"
+                      class="bi bi-file-person"
                       style={{ fontSize: "1.5rem" }}
-                    />
+                    ></i>
                   </div>
                   <div className="d-flex flex-column text-start">
                     <div className="row fw-bold">
-                      <span className="label-small">Check-out</span>
+                      <span className="label-small">Habitaciones</span>
                     </div>
                     <div className="row">
-                      <span className="body-small">Añadir fecha</span>
+                      <span className="body-small">Cantidad</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              {/*Botón de Huespedes*/}
+              <div className="d-flex justify-content-center booking-form-btn input-group input-custom border ms-3">
+                <div className="d-flex flex-row">
+                  <div className="d-flex flex-column me-3 justify-content-center">
+                    <i class="bi bi-people" style={{ fontSize: "1.5rem" }}></i>
+                  </div>
+                  <div className="d-flex flex-column text-start">
+                    <div className="row fw-bold">
+                      <span className="label-small">Huéspedes</span>
+                    </div>
+                    <div className="row">
+                      <span className="body-small">Cantidad</span>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
 
-            <div className="d-flex justify-content-center p-0 mx-md-2 mt-2 mt-md-0">
-              {/*Botón de Habitaciones*/}
-              <div className="border flex-grow-1">
-                <Link className="btn booking-form-btn px-2 py-0 d-flex justify-content-center">
-                  <div className="d-flex flex-row">
-                    <div className="d-flex flex-column me-3 justify-content-center">
-                      <i
-                        class="bi bi-file-person"
-                        style={{ fontSize: "1.5rem" }}
-                      ></i>
-                    </div>
-                    <div className="d-flex flex-column text-start">
-                      <div className="row fw-bold">
-                        <span className="label-small">Habitaciones</span>
-                      </div>
-                      <div className="row">
-                        <span className="body-small">Cantidad</span>
-                      </div>
-                    </div>
-                  </div>
-                </Link>
-              </div>
-              {/*Huespedes*/}
-              <div className="flex-grow-1 border ms-2">
-                <Link className="btn booking-form-btn px-2 py-0 d-flex justify-content-center">
-                  <div className="d-flex flex-row">
-                    <div className="d-flex flex-column me-3 justify-content-center">
-                      <i
-                        class="bi bi-people"
-                        style={{ fontSize: "1.5rem" }}
-                      ></i>
-                    </div>
-                    <div className="d-flex flex-column text-start">
-                      <div className="row fw-bold">
-                        <span className="label-small">Huéspedes</span>
-                      </div>
-                      <div className="row">
-                        <span className="body-small">Cantidad</span>
-                      </div>
-                    </div>
-                  </div>
-                </Link>
-              </div>
-            </div>
             {/*Llamado a la acción*/}
             <div className="d-flex flex-column mt-md-0 mt-2">
               <Button
                 name="Reservar Ahora"
                 btnCustom="solid-btn-primary"
                 btnText="label-small"
-                paddingBtn=""
-                marginBtn=""
                 btnType="submit"
                 btnSize="h-100"
               />
