93ba175549ce0e1b397feec2e6a56ef475bc2337
Jimena Sánchez <mjsp674@hotmail.com>
2025-05-07 15:19:01 -0500

    Se crea la conexión a la base de datos db.js

---
+++ server/config/db.js
@@ -0,0 +1,23 @@
+// Importar modulos para conectarse a la base de datos
+const db = require("mysql2");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+// Conexión a la base de datos
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error("Error al conectarse a la base de datos.", err);
+    return;
+  }
+  console.log("Conectado a la base de datos.");
+});
+
+module.exports = connection;
