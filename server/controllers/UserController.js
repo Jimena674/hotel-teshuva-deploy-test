85b757c1b470d46fc754f4b20253a8be18687567
Jimena674 <mjsp674@hotmail.com>
2025-05-07 19:14:34 -0500

    Se crea la función para crear un usuario en UserController.js

---
+++ server/controllers/UserController.js
@@ -0,0 +1,68 @@
+// Importar módulos para gestionar las req, res, errores y lógica de la conexión a la bd
+const bcrypt = require("bcrypt");
+const userModel = require("../models/UserModel");
+
+// Función para crear un usuario
+const register = async function (req, res) {
+  try {
+    // Datos enviados por el usuario
+    const {
+      name,
+      last_name,
+      id_type_id,
+      id_number,
+      phone_prefixes_id,
+      phone,
+      birth_date,
+      email,
+      user_type_id,
+      password,
+    } = req.body;
+    // Validar la entrada de datos
+    if (
+      !name ||
+      !last_name ||
+      !id_type_id ||
+      !id_number ||
+      !phone_prefixes_id ||
+      !phone ||
+      !birth_date ||
+      !email ||
+      !user_type_id ||
+      !password
+    ) {
+      return res
+        .status(400)
+        .json({ error: "Todos los campos son requeridos." });
+    }
+    // Verificar si el usuario ya existe
+    const user = await userModel.findUserByEmail(email);
+    if (user) {
+      return res.status(409).json({ error: "El usuario ya existe." });
+    }
+    // Encriptar la contraseña
+    const clearPassword = String(password).trim();
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(clearPassword, salt);
+    // Crear usuario
+    await userModel.createUser(
+      name,
+      last_name,
+      id_type_id,
+      id_number,
+      phone_prefixes_id,
+      phone,
+      birth_date,
+      email,
+      user_type_id,
+      hashedPassword
+    );
+
+    res.json({ message: "Usuario creado con éxito." });
+  } catch (error) {
+    console.error("Error al crear el usuario.", error);
+    res.status(500).json({ error: "Error en el servidor." });
+  }
+};
+
+module.exports = { register };
