8165301a9107518fe73fe97c3d1f15eced10f701
Jimena674 <mjsp674@hotmail.com>
2025-05-24 20:03:57 -0500

    Se empiezan a desarrollar las funciones de crear, leer, eliminar y modificar reservas desde el backend.

--- server/controllers/BookingController.js
+++ server/controllers/BookingController.js
@@ -1,4 +1,18 @@
 const bookingModel = require("../models/BookingModel");
+const db = require("../config/db");
+
+/* Función para crear un código de reserva */
+
+function bookingCode(length = 8) {
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let result = "";
+
+  for (let i = 0; i < length; i++) {
+    const index = Math.floor(Math.random() * characters.length);
+    result += characters[index];
+  }
+  return result;
+}
 
 /* Controlador para crear una reserva */
 
@@ -6,27 +20,33 @@ const createBooking = async (req, res) => {
   try {
     // Datos de la solicitud
 
-    const { id_user, check_in, check_out, total, code } = req.body;
+    const { id_user, check_in, check_out, total } = req.body;
 
     // Validar que se hayan ingresado todos los datos
 
-    if (!id_user || !check_in || !check_out || !total || !code) {
+    if (!id_user || !check_in || !check_out || !total) {
       return res
         .status(400)
         .json({ message: "Todos los campos son obligatorios." });
     }
 
-    // Verificar que la reserva no exista
+    let code;
+    let uniqueCode = false;
 
-    const booking = await bookingModel.findBooking(code);
-
-    if (booking) {
-      return res.status(409).json({ message: "La reserva ya existe." });
+    while (!uniqueCode) {
+      code = bookingCode();
+      const [exist] = await db
+        .promise()
+        .query(`SELECT id_booking FROM booking WHERE code = ?`, [code]);
+      if (exist.length === 0) {
+        uniqueCode = true;
+      }
     }
 
     // Enviar los datos a la base de datos
 
-    await bookingModel.createBooking(id_user, check_in, check_out, total, code);
+    const bookingData = { id_user, check_in, check_out, total, code };
+    await bookingModel.createBooking(bookingData);
     res.status(200).json({ message: "Reserva creada con éxito." });
   } catch (error) {
     console.error("Error al crear la reserva: ", error);
@@ -36,19 +56,84 @@ const createBooking = async (req, res) => {
 
 /* Controlador para leer todas las reservas */
 
-const readAllBookings = async (req, res) => {};
+const readAllBookings = async (req, res) => {
+  try {
+    const getAll = await bookingModel.readAllBookings();
+    res.status(200).json(getAll);
+  } catch (error) {
+    console.error("Error al leer los datos de todas las reservas: ", error);
+    res.status(500).json({ error: "Error en el servidor." });
+  }
+};
 
 /* Controlador para leer una reserva */
 
-const readBooking = async (req, res) => {};
+const readBooking = async (req, res) => {
+  try {
+    const idBooking = req.params.id_booking;
+    const booking = await bookingModel.readBooking(idBooking);
+    res.status(200).json(booking);
+  } catch (error) {
+    console.error("Error al leer los datos de la reserva: ", error);
+    res.status(500).json({ error: "Error en el servidor" });
+  }
+};
 
 /* Controlador para eliminar una reserva */
 
-const deleteBooking = async (req, res) => {};
+const deleteBooking = async (req, res) => {
+  try {
+    const idBooking = req.params.id_booking;
+    const result = await bookingModel.deleteBooking(idBooking);
+    res.status(200).json({ message: "Usuario eliminado exitosamente" });
+  } catch (error) {
+    console.error("Error al eliminar la reserva: ", error);
+    res.status(500).json({ error: "Erro en el servidor" });
+  }
+};
 
 /* Controlador para modificar una reserva */
 
-const updateBooking = async (req, res) => {};
+const updateBooking = async (req, res) => {
+  const idBooking = req.params.id_booking;
+  const data = req.body;
+  if (!data) {
+    res
+      .status(400)
+      .json({ message: "No se han proporcionado datos para actualizar." });
+  }
+
+  try {
+    const { id_user, check_in, check_out, total } = req.body;
+
+    if (id_user) {
+      data.id_user = parseInt(id_user);
+    }
+    if (check_in) {
+      data.check_in = check_in;
+    }
+    if (check_out) {
+      data.check_out = check_out;
+    }
+    if (total) {
+      data.total = parseFloat(total);
+    }
+
+    delete data.user_name;
+
+    if (Object.keys(data).length === 0) {
+      return res
+        .status(400)
+        .json({ message: "No se proporcionaron datos válidos." });
+    }
+
+    const result = await bookingModel.updateBooking(idBooking, data);
+    res.status(200).json({ message: "Reserva actualizada con éxito." });
+  } catch (error) {
+    console.error("Error al modificar la reserva: ", error);
+    res.status(500).json({ error: "Error en el servidor." });
+  }
+};
 
 module.exports = {
   createBooking,
--- server/models/BookingModel.js
+++ server/models/BookingModel.js
@@ -2,51 +2,68 @@ const db = require("../config/db");
 
 /* Modelo para crear una reserva */
 
-const createBooking = async (id_user, check_in, check_out, total, code) => {
+const createBooking = async (data) => {
+  const { id_user, check_in, check_out, total, code } = data;
   await db
     .promise()
     .query(
-      `INSERT INTO booking (id_user, check_in, check_out, total, code) VALUES (?, ?, ?, ?, ?)`,
+      `INSERT INTO booking ( id_user, check_in, check_out, total, code) VALUES ( ?, ?, ?, ?, ?)`,
       [id_user, check_in, check_out, total, code]
     );
-};
-
-/* Buscar una reserva por el código */
-
-const findBooking = async (code) => {
-  const [rows] = await db
-    .promise()
-    .query(`SELECT booking WHERE booking.code = ?`, [code]);
-  return rows[0];
 };
 
 /* Modelo para leer todas las reserva */
 
 const readAllBookings = async () => {
-  await db.promise().query();
+  const [rows] = await db
+    .promise()
+    .query(
+      `SELECT booking.check_in, booking.check_out, booking.total, booking.code, user.name AS user_name FROM booking JOIN user ON booking.id_user = user.name `
+    );
+  return rows;
 };
 
-/* Modelo para leer una reserva */
+/* Modelo para leer una reserva a partir del id_booking*/
 
-const readBooking = async () => {
-  await db.promise().query();
+const readBooking = async (id_booking) => {
+  const [rows] = await db
+    .promise()
+    .query(
+      `SELECT booking .*, user.name AS user_name FROM booking JOIN user ON booking.id_user = user.name WHERE id_booking = ?`,
+      [id_booking]
+    );
+  return rows[0];
 };
 
-/* Modelo para eliminar una reserva */
+/* Modelo para eliminar una reserva por el id_booking */
 
-const deleteBooking = async () => {
-  await db.promise().query();
+const deleteBooking = async (id_booking) => {
+  const [result] = await db
+    .promise()
+    .query(`DELETE FROM booking WHERE id_booking = ?`, [id_booking]);
+  return result;
 };
 
 /* Modelo para modificar una reserva */
 
-const updateBooking = async () => {
-  await db.promise().query();
+const updateBooking = async (id_booking, data) => {
+  const fields = [];
+  const values = [];
+
+  for (let key in data) {
+    fields.push(`${key} = ?`);
+    values.push(data[key]);
+  }
+
+  const sql = `UPDATE booking SET ${fields.join(", ")} WHERE id_booking = ?`;
+  values.push(id_booking);
+
+  const [result] = await db.promise().query(sql, values);
+  return result;
 };
 
 module.exports = {
   createBooking,
-  findBooking,
   readAllBookings,
   readBooking,
   deleteBooking,
